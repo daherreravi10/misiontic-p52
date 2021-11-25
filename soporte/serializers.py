0eca4fa9d80072366177f096126a32d7d6bf674f
Daniel herrera <daniel.herreravi@hotmail.com>
2021-11-24 23:48:20 -0500

    initial commit

---
+++ soporte/serializers.py
@@ -0,0 +1,13 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import PQR,PersonaSoporte
+
+class PersonaSoporteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= PersonaSoporte
+        fields= ' all '
+
+class PQRSerializer( serializers.ModelSerializer):
+    class Meta:
+        model= PQR
+        fields= ['persona_soporte','estado', 'comentario']
