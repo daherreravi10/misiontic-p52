4eaa3c8bc3422ad67d396c14c07e90644d40ab82
Daniel herrera <daniel.herreravi@hotmail.com>
2021-11-25 23:25:42 -0500

    New Urls for cruds

---
+++ soporte/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from soporte.views import PQRListCreate, PQRUpdateDelete, PersonaSoporteListCreate, PersonaSoporteUpdateDelete
+
+urlpatterns =[
+    path('persona-soporte/', PersonaSoporteListCreate.as_view),
+    path('persona.soporte/<pk>', PersonaSoporteUpdateDelete.as_view),
+    path('pqr/', PQRListCreate.as_view),
+    path('pqr/<pk>', PQRUpdateDelete.as_view),
+    ]
--- soporte/views.py
+++ soporte/views.py
@@ -1,3 +1,22 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.serializers import Serializer
+from .models import PQR,PersonaSoporte
+from .serializers import PQRSerializer, PersonaSoporteSerializer
 # Create your views here.
+class PersonaSoporteListCreate(generics.ListCreateAPIView):
+    QuerySet= PersonaSoporte.objects.all()
+    serializer_class= PersonaSoporteSerializer
+
+class PersonaSoporteUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    QuerySet = PersonaSoporte.objects.all()
+    serializer_class = PersonaSoporteSerializer
+
+class PQRListCreate(generics.ListCreateAPIView):
+    QuerySet= PQR.objects.all()
+    serializer_class= PQRSerializer
+
+class PQRUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    QuerySet = PQR.objects.all()
+    serializer_class = PQRSerializer
