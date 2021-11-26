1b237c449b269cee3619feb88b8fc356ef755b65
Daniel herrera <daniel.herreravi@hotmail.com>
2021-11-25 23:47:17 -0500

    Arreglo busg

--- soporte/urls.py
+++ soporte/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from soporte.views import PQRListCreate, PQRUpdateDelete, PersonaSoporteListCreate, PersonaSoporteUpdateDelete
 
 urlpatterns =[
-    path('persona-soporte/', PersonaSoporteListCreate.as_view),
-    path('persona.soporte/<pk>/', PersonaSoporteUpdateDelete.as_view),
+    path('personas-soporte/', PersonaSoporteListCreate.as_view),
+    path('personas-soporte/<pk>/', PersonaSoporteUpdateDelete.as_view),
     path('pqr/', PQRListCreate.as_view),
     path('pqr/<pk>/', PQRUpdateDelete.as_view),
     ]
