0b5d05ab91380e4e060a623172b27436b1dc5f84
Daniel herrera <daniel.herreravi@hotmail.com>
2021-11-25 23:42:32 -0500

    Anexo a urls principal

--- soporte/urls.py
+++ soporte/urls.py
@@ -4,7 +4,7 @@ from soporte.views import PQRListCreate, PQRUpdateDelete, PersonaSoporteListCrea
 
 urlpatterns =[
     path('persona-soporte/', PersonaSoporteListCreate.as_view),
-    path('persona.soporte/<pk>', PersonaSoporteUpdateDelete.as_view),
+    path('persona.soporte/<pk>/', PersonaSoporteUpdateDelete.as_view),
     path('pqr/', PQRListCreate.as_view),
-    path('pqr/<pk>', PQRUpdateDelete.as_view),
+    path('pqr/<pk>/', PQRUpdateDelete.as_view),
     ]
